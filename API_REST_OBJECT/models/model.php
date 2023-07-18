91e4b872c18646a396ce20592a894eefa8d72f90
fouuuadi <fouadlamnaouar18@gmail.com>
2023-07-18 19:54:49 +0200

    model finis prochaine etape faire les routes et ensuite les controller

--- API_REST_OBJECT/models/model.php
+++ API_REST_OBJECT/models/model.php
@@ -149,7 +149,7 @@ class ProfileUser{
         $request = $connection->prepare("select * from Users");
         $request->execute();
 
-        $rows = $request->fetchAll(PDO::FETCH-ASSOC);
+        $rows = $request->fetchAll(PDO::FETCH_ASSOC);
         $users = [];
 
         foreach($rows as $row){
@@ -167,6 +167,61 @@ class ProfileUser{
         }
         return $users;
 
+    }
+
+    public function updateUser() {
+        $db = new Database();
+        $connection = $db->getConnection();
+
+        $query = "UPDATE Users SET username = :username, firstname = :firstname, lastname = :lastname, age = :age, description = :description, sexe = :sexe WHERE id = :id";
+
+        $request = $connection->prepare($query);
+        $request->bindParam(':id', $this->id);
+        $request->bindParam(':username', $this->username);
+        $request->bindParam(':firstname', $this->firstname);
+        $request->bindParam(':lastname', $this->lastname);
+        $request->bindParam(':age', $this->age);
+        $request->bindParam(':description', $this->description);
+        $request->bindParam(':sexe', $this->sexe);
+
+        return $request->execute();
+    }
+    public function getUserById($id) {
+        $db = new Database();
+        $connection = $db->getConnection();
+
+        $query = "SELECT * FROM Users WHERE id = :id";
+        $request = $connection->prepare($query);
+        $request->bindParam(':id', $id);
+        $request->execute();
+
+        $row = $request->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            $user = new ProfileUser(
+                $row["id"],
+                $row["username"],
+                $row["firstname"],
+                $row["lastname"],
+                $row["age"],
+                $row["description"],
+                $row["sexe"]
+            );
+            return $user;
+        } else {
+            return null;
+        }
+    }
+
+    public function deleteUser() {
+        $db = new Database();
+        $connection = $db->getConnection();
+
+        $query = "DELETE FROM Users WHERE id = :id";
+        $request = $connection->prepare($query);
+        $request->bindParam(':id', $this->id);
+
+        return $request->execute();
     }
 
 
